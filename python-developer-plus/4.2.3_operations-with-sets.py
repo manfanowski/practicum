4db613f72d6cc8a4fa538f62d178d997d255f6c4
Alexei Kuznetsov <64271316+hhofn@users.noreply.github.com>
2023-01-12 00:00:32 +0100

    4.2.3 Operations with sets

---
+++ python-developer-plus/4.2.3_operations-with-sets.py
@@ -0,0 +1,23 @@
+def get_together_games(games_1, games_2):
+    # Напишите здесь код функции для поиска пересечений
+    together_games = set(games_1).intersection(set(games_2))
+    return together_games
+
+anfisa_games = [
+    'Online-chess',
+    'Города',
+    'DOOM',
+    'Крестики-нолики'
+]
+alisa_games = [
+    'DOOM',
+    'Online-chess',
+    'Города',
+    'GTA',
+    'World of tanks'
+]
+# Вызовите функцию со списками игр в качестве параметров
+together_games = get_together_games(anfisa_games, alisa_games)
+# Напечатайте итоговый перечень игр в цикле
+for game in together_games:
+    print('👾 ' + game)
