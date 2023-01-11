3f96bdd95019a4614225c0695ae1a02bdcb24779
Alexei Kuznetsov <64271316+hhofn@users.noreply.github.com>
2023-01-11 23:29:21 +0100

    2.10.2 Logical expressions

---
+++ python-developer-plus/2.10.2_logical-expressions.py
@@ -0,0 +1,10 @@
+# Добавьте новые условия в elif и else
+for messages_count in range(0, 21):
+    if messages_count == 0:
+        print('У вас нет новых сообщений')
+    elif messages_count == 1:
+        print('У вас ' + str(messages_count) + ' новое сообщение')
+    elif messages_count >= 2 and messages_count <= 4:
+        print('У вас ' + str(messages_count) + ' новых сообщения')
+    else:
+        print('У вас ' + str(messages_count) + ' новых сообщений')
