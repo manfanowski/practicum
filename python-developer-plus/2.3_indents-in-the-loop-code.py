64079ca1992c67578790a2ce39df2d784ce98548
Alexei Kuznetsov <64271316+hhofn@users.noreply.github.com>
2023-01-11 23:05:36 +0100

    Update 2.3_indents-in-the-loop-code.py

--- python-developer-plus/2.3_indents-in-the-loop-code.py
+++ python-developer-plus/2.3_indents-in-the-loop-code.py
@@ -2,5 +2,5 @@ kids = ['Витя', 'Маша', 'Марина']
 candies = ['Батончик', 'Сникерс', 'Мишка Косолапый', 'Коровка']
 
 for kid in kids:
-for candy in candies:
-print(kid, 'получает конфету', candy)
+    for candy in candies:
+        print(kid, 'получает конфету', candy)
