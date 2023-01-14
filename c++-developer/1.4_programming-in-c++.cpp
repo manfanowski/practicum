6c8335d14859e302ad6c870efc61a3636e1042ee
Alexei Kuznetsov <64271316+hhofn@users.noreply.github.com>
2023-01-14 01:02:47 +0100

    1.4 Programming in C++

---
+++ c++-developer/1.4_programming-in-c++.cpp
@@ -0,0 +1,8 @@
+cout << "_____________"s << endl;
+cout << "|  /\"\"\"\"\"\\  |"s << endl;
+cout << "| /       \\ |"s << endl;
+cout << "| | 0   0 | |"s << endl;
+cout << "| |   v   | |"s << endl;
+cout << "| \\  ___  / |"s << endl;
+cout << "|  \\_____/  |"s << endl;
+cout << "-------------"s << endl;
