98e3b262141cdffc3a64677557015851b71d626d
Alexei Kuznetsov <64271316+hhofn@users.noreply.github.com>
2023-04-03 13:26:43 -0700

    Create 4.2_data-entry.cpp

---
+++ c++-developer/4.2_data-entry.cpp
@@ -0,0 +1,9 @@
+cout << "Enter x and y:"s << endl;
+
+double x, y;
+cin >> x >> y;
+
+cout << "x + y = "s << (x + y) << endl;
+cout << "x - y = "s << (x - y) << endl;
+cout << "x * y = "s << (x * y) << endl;
+cout << "x / y = "s << (x / y) << endl;
